2b98b5ad0cd73c53343717640485f9879d35a6f9
Stephanie <stranosteph@gmail.com>
2023-12-07 21:02:55 -0500

    Stored messages

---
+++ server/src/main/java/com/chatapp/Chat/App/service/MessageService.java
@@ -0,0 +1,44 @@
+package com.chatapp.Chat.App.service;
+
+import com.chatapp.Chat.App.model.Channel;
+import com.chatapp.Chat.App.model.Message;
+import com.chatapp.Chat.App.model.auth.User;
+import com.chatapp.Chat.App.payload.request.MessageDTO;
+import com.chatapp.Chat.App.repository.MessageRepository;
+import com.chatapp.Chat.App.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Service
+public class MessageService {
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Autowired
+    UserRepository userRepository;
+    public Message save(Long channelId, MessageDTO message, Principal principal) {
+        User currentUser = getUser(principal);
+
+        Message newMessage = new Message(new Channel(channelId), currentUser, message.getContent());
+        Message saveMessage = messageRepository.save(newMessage);
+        Optional<Message> retrieveMessage = messageRepository.findById(saveMessage.getId());
+        if (retrieveMessage.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Message could not be found");
+        }
+        return retrieveMessage.get();
+    }
+
+    private User getUser(Principal principal) {
+        Optional<User> user = userRepository.findByUsername(principal.getName());
+        if (user.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Logged in user does not exist");
+
+        }
+        return user.get();
+    }
+}
