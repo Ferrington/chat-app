344d461093c1d6a97b7eaac28a77ee204b47b0ee
Steve Her <122183092+Sth84478@users.noreply.github.com>
2023-12-10 08:49:05 -0600

    Added path variable to message controller and put a try-catch in method.

--- server/src/main/java/com/chatapp/Chat/App/controller/REST/MessageController.java
+++ server/src/main/java/com/chatapp/Chat/App/controller/REST/MessageController.java
@@ -4,12 +4,12 @@ import com.chatapp.Chat.App.model.Channel;
 import com.chatapp.Chat.App.model.Message;
 import com.chatapp.Chat.App.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,9 +21,16 @@ public class MessageController {
     @Autowired
     MessageService messageService;
 
-    @GetMapping
-    public List<Message> getAllMessagesInChannel(Channel channelId) {
-        return messageService.getAllMessagesInChannel(channelId);
+    @GetMapping(path= "/{channelId}")
+    public List<Message> getAllMessagesInChannel(@PathVariable Channel channelId) {
+        List<Message> channelMessages = new ArrayList<>();
+
+        try {
+            channelMessages = messageService.getAllMessagesInChannel(channelId);
+        } catch (Error e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+        return channelMessages;
     }
 
 }
