8cc4cba4dad855b7f9f495237bc0f469ade086e3
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-12 19:40:56 -0600

    test added

--- server/src/test/java/com/chatapp/Chat/App/controller/MessageControllerTests.java
+++ server/src/test/java/com/chatapp/Chat/App/controller/MessageControllerTests.java
@@ -3,15 +3,27 @@ package com.chatapp.Chat.App.controller;
 import com.chatapp.Chat.App.model.Channel;
 import com.chatapp.Chat.App.model.Message;
 import com.chatapp.Chat.App.service.MessageService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,22 +42,27 @@ public class MessageControllerTests {
     static private final LocalDateTime MESSAGE_3_TIME = LocalDateTime.of(2023, 9, 25, 6, 30, 5);
     static private final LocalDateTime MESSAGE_4_TIME = LocalDateTime.of(2023, 11, 25, 6, 30, 5);
 
+    static private final List<Message> MESSAGES = new ArrayList<>();
+
     static {
         MESSAGE_1.setChannel(CHANNEL_ID_1);
         MESSAGE_1.setContent("test content first");
-        MESSAGE_1.setUpdated(MESSAGE_1_TIME);
+        MESSAGE_1.setCreated(MESSAGE_1_TIME);
 
         MESSAGE_2.setChannel(CHANNEL_ID_1);
         MESSAGE_2.setContent("test content second");
-        MESSAGE_2.setUpdated(MESSAGE_2_TIME);
+        MESSAGE_2.setCreated(MESSAGE_2_TIME);
 
         MESSAGE_3.setChannel(CHANNEL_ID_1);
         MESSAGE_3.setContent("test content third");
-        MESSAGE_3.setUpdated(MESSAGE_3_TIME);
+        MESSAGE_3.setCreated(MESSAGE_3_TIME);
 
         MESSAGE_4.setChannel(CHANNEL_ID_1);
         MESSAGE_4.setContent("test content fourth");
-        MESSAGE_4.setUpdated(MESSAGE_4_TIME);
+        MESSAGE_4.setCreated(MESSAGE_4_TIME);
+
+        MESSAGES.add(MESSAGE_1);
+        MESSAGES.add(MESSAGE_2);
     }
 
     @MockBean
@@ -54,18 +71,14 @@ public class MessageControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-//    @Test
-//    @WithMockUser
-//    public void getAllMessagesInChannel_endpoint_mapped() throws Exception {
-//        Page<Message> messages = messageService.getAllMessagesInChannel(CHANNEL_ID_1, PAGE, SIZE);
-//
-//        when(messageService.getAllMessagesInChannel(CHANNEL_ID_1, PAGE, SIZE)).thenReturn(messages);
-//
-//        mockMvc.perform(get("/api/messages/{channelId}", CHANNEL_ID_1.getId()))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.content[0].content").value("test content second"))
-//                .andExpect(jsonPath("$.content[1].content").value("test content fourth"));
-//
-//    }
+    @Test
+    @WithMockUser
+    public void getMessagesFromChannel_endpoint_mapped() throws Exception {
+        when(messageService.getMessagesFromChannel(any(), any(), anyInt(), any())).thenReturn(MESSAGES);
 
+        mockMvc.perform(get("/api/messages/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].content").value("test content first"));
+    }
 }
