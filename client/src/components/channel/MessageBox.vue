dffd0aa4c647970cd8dbab3584176fe487ab732d
jamesdrichardson <jdr@jamesdrichardson.dev>
2023-12-19 16:08:55 -0500

    prototype for viewing will improve if it is accepted

--- client/src/components/channel/MessageBox.vue
+++ client/src/components/channel/MessageBox.vue
@@ -4,15 +4,63 @@ import { ref } from 'vue';
 
 const { sendMessage } = useSocketStore();
 const text = ref('');
+
+//clears the message box.
+function clear(){
+  text.value = '';
+};
+
+// function boxHeight(){
+//   const messageHeight = 
+// }
 </script>
 
 <template>
+  <!--have to figure out a way to vertically dynamically change the height of the message box....-->
   <div>
-    <form @submit.prevent="sendMessage(text)">
-      <input type="text" v-model="text" />
+      <!-- changed input type text to text box. made it so that you can press enter to send message (not native on text box) as well as you
+      can use shift enter to make a line break old code below also commented out-->
+      <!-- <input type="text" v-model="text" id="messageBox" placeholder="Message Channel..."/> -->
+      <form @submit.prevent="sendMessage(text)" @submit="clear" id="messageArea">
+        <!--add variable to change channel to what ever or whoever youre messaging-->
+      <textarea name="messageBox" id="messageBox" cols="30" rows="1" placeholder="Message Channel..." auto-grow
+      v-model="text" v-on:keydown.enter.exact.prevent="{sendMessage(text);clear();}"></textarea>
+      <!--find a way to incorparate the button? make it look cooler? -->
       <button type="submit">Send</button>
     </form>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+/* styled where the message and button make a flexbox so that they are always displayed on the same line.
+ have to figure out the styling on the main page to get this functional at smaller sizes though*/
+
+#messageBox {
+  /* height: scrollHeight; */
+  text-align: left;
+  flex-grow: 1;
+  overflow-wrap: normal;
+  resize: none;
+  max-height: 200px; 
+  border-style: none;
+  outline: none;
+}
+
+#messageArea{
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  border-top: 2px solid black;
+}
+
+button{
+  border-style: none;
+  background-color: white;
+  display: inline-flex;
+  align-items: flex-end;
+  padding-bottom: 15px;
+  padding-right: 15px;
+}
+
+</style>
