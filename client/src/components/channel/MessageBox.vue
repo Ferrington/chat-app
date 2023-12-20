8af0195404d68b9ca7caa1f5b3565f012a55c795
jamesdrichardson <jdr@jamesdrichardson.dev>
2023-12-20 13:18:08 -0500

    styling stuff and some functionality

--- client/src/components/channel/MessageBox.vue
+++ client/src/components/channel/MessageBox.vue
@@ -17,16 +17,17 @@ function clear(){
 
 <template>
   <!--have to figure out a way to vertically dynamically change the height of the message box....-->
-  <div>
+  <div id="messageDiv">
       <!-- changed input type text to text box. made it so that you can press enter to send message (not native on text box) as well as you
       can use shift enter to make a line break old code below also commented out-->
       <!-- <input type="text" v-model="text" id="messageBox" placeholder="Message Channel..."/> -->
-      <form @submit.prevent="sendMessage(text)" @submit="clear" id="messageArea">
+    <form @submit.prevent="sendMessage(text)" @submit="clear" id="messageArea">
         <!--add variable to change channel to what ever or whoever youre messaging-->
       <textarea name="messageBox" id="messageBox" cols="30" rows="1" placeholder="Message Channel..." auto-grow
       v-model="text" v-on:keydown.enter.exact.prevent="{sendMessage(text);clear();}"></textarea>
       <!--find a way to incorparate the button? make it look cooler? -->
-      <button type="submit"><img src="/Users/jamesrichardson/Desktop/Chat App/chat-app/client/src/assets/images/testicon.png" alt="Send Message" id="sendImg"></button>
+      <!--OLD IMAGE PLACEHOLDER <img src="/Users/jamesrichardson/Desktop/Chat App/chat-app/client/src/assets/images/testicon.png" alt="Send Message" id="sendImg">-->
+      <button type="submit"><font-awesome-icon :icon="['fas', 'paper-plane']" /></button>
     </form>
   </div>
 </template>
@@ -35,6 +36,27 @@ function clear(){
 
 /* styled where the message and button make a flexbox so that they are always displayed on the same line.
  have to figure out the styling on the main page to get this functional at smaller sizes though*/
+
+#messageDiv {
+  background-color: white;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+#messageArea{
+  display: flex;
+  flex-direction: row;
+  height: 75%;
+  /* border-top: 2px solid black; */
+  border: 1px solid lightgray;
+  background-color: lightgray;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  
+  
+}
 
 #messageBox {
   /* height: scrollHeight; */
@@ -45,25 +67,31 @@ function clear(){
   max-height: 200px; 
   border-style: none;
   outline: none;
-}
+  background-color: inherit;
+  border-right: none;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  padding-top: 10px;
+  padding-left: 15px;
+  padding-right: 50px;
 
-#messageArea{
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-  border-top: 2px solid black;
 }
 
 button{
-  border-style: none;
-  background-color: white;
-  display: inline-flex;
-  align-items: flex-end;
-  padding-bottom: 15px;
-  padding-right: 15px;
+  border-left-style: none;
+  /* display: inline-flex;
+  align-items: flex-end; */
+  position: absolute;
+  bottom: 30px;
+  right: 25px;
+  background-color: inherit;
+  border: none;
+  
 }
-#sendImg{
+/* #sendImg{
   width: 25px;
-}
+} */
 
 </style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -7,11 +7,33 @@ import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 
+//font awesome imports.  for more information visit the website in the chat-app discord channel. The beginning of this process will require you to create a .npmrc file
+// which will ensure that your system resolves the "login" with a free license and does not resolve with needing a password. you will also need to run a few npm commands as well
+
+//The first import creates the library where we store the font awesome icons we would like
+//to use for the project. this will be an ever growing list if we continue to use font awesome 
+//as its more space and time efficient than installing and loading the WHOLE thing everytime. 
+//we wont need that many icons...i hope lol. 
+import { library } from '@fortawesome/fontawesome-svg-core';
+//The second line is adding a component into vue such that we can call
+//the icons in any .vue file the syntax when calling looks like this: <font-awesome-icon :icon="['fas', 'paper-plane']" />
+//there are multiple different syntax's you can use but i feel as though this one is the most specific which will lead to less
+//ambiguity and mistakes in its use. Feel free to tell me im wrong though!  I would love to know if theres a better way than what i cobbled together in like 15 mins:)
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+//The third line is addding the specific icon we're looking to use in this case its a paper plane but it will eventually be other things.
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+//this line adds the imported icons into our library. One thing to keep in mind is that you dont need several lines for multiple adds. should be able to do it as csv's 
+//within the parenthetical statement.
+library.add(faPaperPlane);
+
+
 axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API + '/api';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
+
