7c3105aff711332e632615ae4f49c9b70165bee4
yuneKim <yune@yunekim.dev>
2024-01-10 19:12:30 -0500

    added premade chat history in to the data.sql file

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -17,4 +17,29 @@ VALUES
     ('General', 'A general place to chat'),
     ('Games', 'Talk about games'),
     ('Movies', 'Talk about movies'),
-    ('Pets', 'Talk about pets')
+    ('Pets', 'Talk about pets');
+
+INSERT INTO messages (channel_id, user_id, content, created, updated)
+VALUES
+    (1, 1, 'This is a dummy message 1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 5', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 6', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 7', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 8', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 9', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 10', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 11', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 12', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 13', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 14', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 15', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 16', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 17', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 18', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 19', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 20', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+     (1, 1, 'This is a dummy message 21', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 1, 'This is a dummy message 22', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
